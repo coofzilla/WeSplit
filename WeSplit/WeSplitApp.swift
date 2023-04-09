fb7805d7f80ac40527670d34483a0137b655f785
coofzilla <jeric614@yahoo.com>
2023-04-09 15:36:51 +0900

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jeric Hernandez on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
