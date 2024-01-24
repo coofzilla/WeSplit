95a731c7cb9033d58b73123c599a469d52408df4
coofzilla <jeric614@yahoo.com>
2024-01-25 07:32:40 +0900

    feature: use locale to specify users currency

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,18 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    static let students = ["Harry", "Ron", "Hermoine"]
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
 
-    @State private var selectedStudent = students[1]
+    let tipPercentages = [10, 15, 20, 25, 0]
 
     var body: some View {
-        NavigationStack {
-            Form {
-                Picker("Select Student", selection: $selectedStudent) {
-                    ForEach(Self.students, id: \.self) { student in
-                        Text(student).tag(student)
-                    }
-                }
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
             }
         }
     }
